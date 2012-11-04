84f9f0c1c7e27b760e0c8c113bab9189c295ed16
wbond <will@wbond.net>
2012-11-04 02:21:30 -0500

    Fixed a bug with recursion in the basic auth handler when incorrect username and password are supplied

--- Package Control.py
+++ Package Control.py
@@ -36,6 +36,29 @@ if os.name == 'nt':
     from ntlm import HTTPNtlmAuthHandler
 
 
+# Monkey patch AbstractBasicAuthHandler to prevent infinite recursion
+def non_recursive_http_error_auth_reqed(self, authreq, host, req, headers):
+    authreq = headers.get(authreq, None)
+
+    if not hasattr(self, 'retried'):
+        self.retried = 0
+
+    if self.retried > 0:
+        raise urllib2.HTTPError(req.get_full_url(), 401, "basic auth failed",
+            headers, None)
+    else:
+        self.retried += 1
+
+    if authreq:
+        mo = urllib2.AbstractBasicAuthHandler.rx.search(authreq)
+        if mo:
+            scheme, quote, realm = mo.groups()
+            if scheme.lower() == 'basic':
+                return self.retry_http_basic_auth(host, req, realm)
+
+urllib2.AbstractBasicAuthHandler.http_error_auth_reqed = non_recursive_http_error_auth_reqed
+
+
 class DebuggableHTTPResponse(httplib.HTTPResponse):
     """
     A custom HTTPResponse that formats debugging info for Sublime Text
