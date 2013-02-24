f7308780ada2e26ec2aaf08ba70931aad8beb1ed
wbond <will@wbond.net>
2013-02-23 23:37:50 -0500

    Simplified importing command classes

--- Package Control.py
+++ Package Control.py
@@ -24,21 +24,8 @@ if reloader_name in sys.modules:
 try:
 	# Python 3
 	from .package_control import reloader
-
-	from .package_control.commands.add_repository_channel_command import AddRepositoryChannelCommand
-	from .package_control.commands.add_repository_command import AddRepositoryCommand
-	from .package_control.commands.create_binary_package_command import CreateBinaryPackageCommand
-	from .package_control.commands.create_package_command import CreatePackageCommand
-	from .package_control.commands.disable_package_command import DisablePackageCommand
-	from .package_control.commands.discover_packages_command import DiscoverPackagesCommand
-	from .package_control.commands.enable_package_command import EnablePackageCommand
-	from .package_control.commands.install_package_command import InstallPackageCommand
-	from .package_control.commands.list_packages_command import ListPackagesCommand
-	from .package_control.commands.remove_package_command import RemovePackageCommand
-	from .package_control.commands.upgrade_all_packages_command import UpgradeAllPackagesCommand
-	from .package_control.commands.upgrade_package_command import UpgradePackageCommand
-	from .package_control.commands.package_message_command import PackageMessageCommand
-
+	
+	from .package_control.commands import *
 	from .package_control.package_cleanup import PackageCleanup
 	
 except (ValueError):
@@ -46,20 +33,7 @@ except (ValueError):
 	from package_control import reloader
 	from package_control import sys_path
 
-	from package_control.commands.add_repository_channel_command import AddRepositoryChannelCommand
-	from package_control.commands.add_repository_command import AddRepositoryCommand
-	from package_control.commands.create_binary_package_command import CreateBinaryPackageCommand
-	from package_control.commands.create_package_command import CreatePackageCommand
-	from package_control.commands.disable_package_command import DisablePackageCommand
-	from package_control.commands.discover_packages_command import DiscoverPackagesCommand
-	from package_control.commands.enable_package_command import EnablePackageCommand
-	from package_control.commands.install_package_command import InstallPackageCommand
-	from package_control.commands.list_packages_command import ListPackagesCommand
-	from package_control.commands.remove_package_command import RemovePackageCommand
-	from package_control.commands.upgrade_all_packages_command import UpgradeAllPackagesCommand
-	from package_control.commands.upgrade_package_command import UpgradePackageCommand
-	from package_control.commands.package_message_command import PackageMessageCommand
-
+	from package_control.commands import *
 	from package_control.package_cleanup import PackageCleanup
 
 
