84fc99a099952ae4eb9e91fad9b0b3fa094241d8
wbond <will@wbond.net>
2013-03-25 02:17:41 -0400

    Fixed python files to all end in a newline, fixed indenting to always use 4 spaces

--- package_control/open_compat.py
+++ package_control/open_compat.py
@@ -5,22 +5,22 @@ from .file_not_found_error import FileNotFoundError
 
 
 def open_compat(path, mode='r'):
-	if mode in ['r', 'rb'] and not os.path.exists(path):
-		raise FileNotFoundError(u"The file \"%s\" could not be found" % path)
+    if mode in ['r', 'rb'] and not os.path.exists(path):
+        raise FileNotFoundError(u"The file \"%s\" could not be found" % path)
 
-	if int(sublime.version()) > 3000:
-		encoding = 'utf-8'
-		errors = 'replace'
-		if mode in ['rb', 'wb', 'ab']:
-			encoding = None
-			errors = None
-		return open(path, mode, encoding=encoding, errors=errors)
-	
-	else:
-		return open(path, mode)
+    if int(sublime.version()) > 3000:
+        encoding = 'utf-8'
+        errors = 'replace'
+        if mode in ['rb', 'wb', 'ab']:
+            encoding = None
+            errors = None
+        return open(path, mode, encoding=encoding, errors=errors)
+    
+    else:
+        return open(path, mode)
 
 def read_compat(file_obj):
-	if int(sublime.version()) > 3000:
-		return file_obj.read()
-	else:
-		return unicode(file_obj.read(), 'utf-8', errors='replace')
+    if int(sublime.version()) > 3000:
+        return file_obj.read()
+    else:
+        return unicode(file_obj.read(), 'utf-8', errors='replace')
