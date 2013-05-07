f4bff7e7ee054a00d7d758566b4fe2bc3601a98b
wbond <will@wbond.net>
2013-05-07 00:31:19 -0400

    Added detection of improper installation to help users self-diagnose issues

--- Package Control.py
+++ Package Control.py
@@ -1,5 +1,6 @@
 import sublime
 import sys
+import os
 
 
 st_version = 2
@@ -13,38 +14,67 @@ elif int(sublime.version()) > 3000:
     st_version = 3
 
 
-reloader_name = 'package_control.reloader'
-
-# ST3 loads each package as a module, so it needs an extra prefix
 if st_version == 3:
-    reloader_name = 'Package Control.' + reloader_name
-    from imp import reload
-
-# Make sure all dependencies are reloaded on upgrade
-if reloader_name in sys.modules:
-    reload(sys.modules[reloader_name])
+    installed_dir, _ = __name__.split('.')
+elif st_version == 2:
+    installed_dir = os.path.basename(os.getcwd())
 
 
-try:
-    # Python 3
-    from .package_control import reloader
+# Ensure the user has installed Package Control properly
+if installed_dir != 'Package Control':
+    message = (u"Package Control\n\nThis package appears to be installed " +
+        u"incorrectly.\n\nIt should be installed as \"Package Control\", " +
+        u"but seems to be installed as \"%s\".\n\n" % installed_dir)
+    # If installed unpacked
+    if os.path.exists(os.path.join(sublime.packages_path(), installed_dir)):
+        message += (u"Please use the Preferences > Browse Packages... menu " +
+            u"entry to open the \"Packages/\" folder and rename" +
+            u"\"%s/\" to \"Package Control/\" " % installed_dir)
+    # If installed as a .sublime-package file
+    else:
+        message += (u"Please use the Preferences > Browse Packages... menu " +
+            u"entry to open the \"Packages/\" folder, then browse up a " +
+            u"folder and into the \"Installed Packages/\" folder.\n\n" +
+            u"Inside of \"Installed Packages/\", rename " +
+            u"\"%s.sublime-package\" to " % installed_dir +
+            u"\"Package Control.sublime-package\" ")
+    message += u"and restart Sublime Text."
+    sublime.error_message(message)
 
-    from .package_control.commands import *
-    from .package_control.package_cleanup import PackageCleanup
+# Normal execution will finish setting up the package
+else:
+    reloader_name = 'package_control.reloader'
 
-except (ValueError):
-    # Python 2
-    from package_control import reloader
-    from package_control import sys_path
+    # ST3 loads each package as a module, so it needs an extra prefix
+    if st_version == 3:
+        reloader_name = 'Package Control.' + reloader_name
+        from imp import reload
 
-    from package_control.commands import *
-    from package_control.package_cleanup import PackageCleanup
+    # Make sure all dependencies are reloaded on upgrade
+    if reloader_name in sys.modules:
+        reload(sys.modules[reloader_name])
 
 
-def plugin_loaded():
-    # Start shortly after Sublime starts so package renames don't cause errors
-    # with keybindings, settings, etc disappearing in the middle of parsing
-    sublime.set_timeout(lambda: PackageCleanup().start(), 2000)
+    try:
+        # Python 3
+        from .package_control import reloader
 
-if st_version == 2:
-    plugin_loaded()
+        from .package_control.commands import *
+        from .package_control.package_cleanup import PackageCleanup
+
+    except (ValueError):
+        # Python 2
+        from package_control import reloader
+        from package_control import sys_path
+
+        from package_control.commands import *
+        from package_control.package_cleanup import PackageCleanup
+
+
+    def plugin_loaded():
+        # Start shortly after Sublime starts so package renames don't cause errors
+        # with keybindings, settings, etc disappearing in the middle of parsing
+        sublime.set_timeout(lambda: PackageCleanup().start(), 2000)
+
+    if st_version == 2:
+        plugin_loaded()
