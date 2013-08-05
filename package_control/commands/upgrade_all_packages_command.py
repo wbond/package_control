def7ffaa0ac0ab70f84c2226927ad2cf6e055f6d
wbond <will@wbond.net>
2013-08-05 00:34:00 -0400

    Fixed the Upgrade All Packages command to properly re-enable disabled packages when complete

--- package_control/commands/upgrade_all_packages_command.py
+++ package_control/commands/upgrade_all_packages_command.py
@@ -71,7 +71,7 @@ class UpgradeAllPackagesThread(threading.Thread, PackageInstaller):
             package_names = []
             for info in package_list:
                 package_names.append(info[0])
-            disabled_packages = self.disable_packages(package_names)
+            disabled_packages.extend(self.disable_packages(package_names))
             threading.Thread(target=do_upgrades).start()
 
         sublime.set_timeout(disable_packages, 1)
