464fbde11c6c09c7ee281dbfcc50447753eeb5ef
wbond <will@wbond.net>
2013-10-10 23:57:41 -0400

    Fixed package renaming to work for both packed and unpacked packages

--- package_control/package_renamer.py
+++ package_control/package_renamer.py
@@ -3,7 +3,6 @@ import os
 import sublime
 
 from .console_write import console_write
-from .package_io import package_file_exists
 
 
 class PackageRenamer():
@@ -35,6 +34,7 @@ class PackageRenamer():
         # Fetch the packages since that will pull in the renamed packages list
         installer.manager.list_available_packages()
         renamed_packages = installer.manager.settings.get('renamed_packages', {})
+
         if not renamed_packages:
             renamed_packages = {}
 
@@ -43,26 +43,20 @@ class PackageRenamer():
         # There are the packages actually present on the filesystem
         present_packages = installer.manager.list_packages()
 
+        case_insensitive_fs = sublime.platform() in ['windows', 'osx']
+
         # Rename directories for packages that have changed names
         for package_name in renamed_packages:
-            package_dir = os.path.join(sublime.packages_path(), package_name)
-            if not package_file_exists(package_name, 'package-metadata.json'):
-                continue
-
             new_package_name = renamed_packages[package_name]
-            new_package_dir = os.path.join(sublime.packages_path(),
-                new_package_name)
-
             changing_case = package_name.lower() == new_package_name.lower()
-            case_insensitive_fs = sublime.platform() in ['windows', 'osx']
 
             # Since Windows and OSX use case-insensitive filesystems, we have to
             # scan through the list of installed packages if the rename of the
             # package is just changing the case of it. If we don't find the old
             # name for it, we continue the loop since os.path.exists() will return
             # true due to the case-insensitive nature of the filesystems.
+            has_old = False
             if case_insensitive_fs and changing_case:
-                has_old = False
                 for present_package_name in present_packages:
                     if present_package_name == package_name:
                         has_old = True
@@ -70,22 +64,37 @@ class PackageRenamer():
                 if not has_old:
                     continue
 
-            if not os.path.exists(new_package_dir) or (case_insensitive_fs and changing_case):
+            # For handling .sublime-package files
+            package_file = os.path.join(sublime.installed_packages_path(),
+                package_name + '.sublime-package')
+            # For handling unpacked packages
+            package_dir = os.path.join(sublime.packages_path(), package_name)
 
+            if os.path.exists(package_file):
+                new_package_path = os.path.join(sublime.installed_packages_path(),
+                    new_package_name + '.sublime-package')
+                package_path = package_file
+            elif os.path.exists(os.path.join(package_dir, 'package-metadata.json')):
+                new_package_path = os.path.join(sublime.packages_path(),
+                    new_package_name)
+                package_path = package_dir
+            else:
+                continue
+
+            if not os.path.exists(new_package_path) or (case_insensitive_fs and changing_case):
                 # Windows will not allow you to rename to the same name with
                 # a different case, so we work around that with a temporary name
                 if os.name == 'nt' and changing_case:
                     temp_package_name = '__' + new_package_name
-                    temp_package_dir = os.path.join(sublime.packages_path(),
+                    temp_package_path = os.path.join(sublime.packages_path(),
                         temp_package_name)
-                    os.rename(package_dir, temp_package_dir)
-                    package_dir = temp_package_dir
+                    os.rename(package_path, temp_package_path)
+                    package_path = temp_package_path
 
-                os.rename(package_dir, new_package_dir)
+                os.rename(package_path, new_package_path)
                 installed_pkgs.append(new_package_name)
 
                 console_write(u'Renamed %s to %s' % (package_name, new_package_name), True)
-
             else:
                 installer.manager.remove_package(package_name)
                 message_string = u'Removed %s since package with new name (%s) already exists' % (
