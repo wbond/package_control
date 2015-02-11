0e700e6f8d74ee99c5d0f37ac02ef2bed3c0680d
wbond <will@wbond.net>
2015-02-10 23:07:21 -0500

    Consider a dependency missing if the loader isn't present

--- package_control/package_cleanup.py
+++ package_control/package_cleanup.py
@@ -134,7 +134,7 @@ class PackageCleanup(threading.Thread):
                 delete_directory(package_dir)
 
             # Skip over dependencies since we handle them separately
-            if package_file_exists(package_name, 'dependency-metadata.json'):
+            if package_file_exists(package_name, 'dependency-metadata.json') and loader.exists(package_name):
                 found_dependencies.append(package_name)
                 continue
 
