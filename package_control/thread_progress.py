d65406ee9ff65032d4a55689f602c704dc6cb70b
wbond <will@wbond.net>
2015-05-29 08:59:00 -0400

    Change thread progress indicator to only run on the window that the user interaction was started from and to only be displayed on the current view in that window, instead of running as a global status.
    The benefits of this include:
    
     1. The status will only show on the Window that the Quick Panel
        popup will appear on
     2. Sublime Text seems to have poor performance updating the
        global status marker when many windows are open

--- package_control/thread_progress.py
+++ package_control/thread_progress.py
@@ -22,20 +22,35 @@ class ThreadProgress():
         self.success_message = success_message
         self.addend = 1
         self.size = 8
+        self.last_view = None
+        self.window = None
         sublime.set_timeout(lambda: self.run(0), 100)
 
     def run(self, i):
+        if self.window is None:
+            self.window = sublime.active_window()
+        active_view = self.window.active_view()
+
+        if self.last_view is not None and active_view != self.last_view:
+            self.last_view.erase_status('_package_control')
+            self.last_view = None
+
         if not self.thread.is_alive():
+            def cleanup():
+                active_view.erase_status('_package_control')
             if hasattr(self.thread, 'result') and not self.thread.result:
-                sublime.status_message('')
+                cleanup()
                 return
-            sublime.status_message(self.success_message)
+            active_view.set_status('_package_control', self.success_message)
+            sublime.set_timeout(cleanup, 1000)
             return
 
         before = i % self.size
         after = (self.size - 1) - before
 
-        sublime.status_message('%s [%s=%s]' % (self.message, ' ' * before, ' ' * after))
+        active_view.set_status('_package_control', '%s [%s=%s]' % (self.message, ' ' * before, ' ' * after))
+        if self.last_view is None:
+            self.last_view = active_view
 
         if not after:
             self.addend = -1
