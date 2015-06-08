8366aa101ec69cb512948184f4cdb732444fef31
wbond <will@wbond.net>
2015-06-08 10:12:25 -0400

    Fixed the List Packages command action to open Installed Packages if the package was installed packed

--- package_control/commands/list_packages_command.py
+++ package_control/commands/list_packages_command.py
@@ -81,6 +81,8 @@ class ListPackagesThread(threading.Thread, ExistingPackagesCommand):
         package_name = self.package_list[picked][0]
 
         def open_dir():
-            self.window.run_command('open_dir',
-                {"dir": os.path.join(sublime.packages_path(), package_name)})
+            package_dir = self.manager.get_package_dir(package_name)
+            if not os.path.exists(package_dir):
+                package_dir = self.manager.settings['installed_packages_path']
+            self.window.run_command('open_dir', {"dir": package_dir})
         sublime.set_timeout(open_dir, 10)
