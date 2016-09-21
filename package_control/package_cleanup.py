c1b967a2850ee0a199a6c9521aa0c4ccc9c2d356
evandrocoan <evandrocoan@hotmail.com>
2016-09-21 15:26:17 -0300

    Applied version guards as overrides only exist for ST3.

--- package_control/package_cleanup.py
+++ package_control/package_cleanup.py
@@ -152,7 +152,7 @@ class PackageCleanup(threading.Thread):
 
             clean_old_files(package_dir)
 
-            if os.path.exists(package_dir + '/.sublime-package-override' ):
+            if int(sublime.version()) > 3000 and os.path.exists(package_dir + '/.sublime-package-override'):
                 found = False
 
             # Cleanup packages/dependencies that could not be removed due to in-use files
