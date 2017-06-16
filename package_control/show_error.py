9bb8d430a088b362062b4d137da713ccd7df2b59
evandrocoan <evandrocoan@hotmail.com>
2017-06-16 10:10:36 -0300

    When there is a batch performance for some chain of execution, and
    there are error on them, we cannot show a error message dialog for
    each one of them immediately, otherwise we would flood the user
    with messages.
    
    See:
    Sublime Text Files syntax error flooding
    https://github.com/SublimeTextIssues/Core/issues/1510

--- package_control/show_error.py
+++ package_control/show_error.py
@@ -1,7 +1,14 @@
 import sublime
 
 from . import text
+from .console_write import console_write
 
+from threading import Timer
+
+# When there is a batch performance for some chain of execution, and there are error on them,
+# we cannot show a error message dialog for each one of them immediately, otherwise we would
+# flood the user with messages. See: https://github.com/SublimeTextIssues/Core/issues/1510
+is_error_recentely_displayed = False
 
 def show_error(string, params=None, strip=True, indent=None):
     """
@@ -21,5 +28,19 @@ def show_error(string, params=None, strip=True, indent=None):
         If all lines should be indented by a set indent after being dedented
     """
 
+    global is_error_recentely_displayed
     string = text.format(string, params, strip=strip, indent=indent)
-    sublime.error_message(u'Package Control\n\n%s' % string)
+
+    if is_error_recentely_displayed:
+        console_write( string )
+    else:
+        sublime.error_message(u'Package Control\n\n%s' % string)
+        is_error_recentely_displayed = True
+
+        # Enable the message dialog after x.x seconds
+        thread = Timer(60.0, _restart_error_messages)
+        thread.start()
+
+def _restart_error_messages():
+    global is_error_recentely_displayed
+    is_error_recentely_displayed = False
