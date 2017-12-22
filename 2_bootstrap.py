b5a668d2f4ee8324c2e7595d502b6f8191cecc99
DeathAxe <deathaxe@web.de>
2017-12-22 19:07:12 +0100

    Remove py2 support from '2_bootstap' module

--- 2_bootstrap.py
+++ 2_bootstrap.py
@@ -1,37 +1,23 @@
-import sys
 import threading
-import os
+
 from textwrap import dedent
 
 import sublime
 
-# Clean up the installed and pristine packages for Package Control 2 to
-# prevent a downgrade from happening via Sublime Text
-if sys.version_info < (3,):
-    sublime_dir = os.path.dirname(sublime.packages_path())
-    pristine_dir = os.path.join(sublime_dir, 'Pristine Packages')
-    installed_dir = os.path.join(sublime_dir, 'Installed Packages')
-    pristine_file = os.path.join(pristine_dir, 'Package Control.sublime-package')
-    installed_file = os.path.join(installed_dir, 'Package Control.sublime-package')
-    if os.path.exists(pristine_file):
-        os.remove(pristine_file)
-    if os.path.exists(installed_file):
-        os.remove(installed_file)
+st_build = int(sublime.version())
+if st_build < 3000:
+    raise ImportError('Package Control requires Sublime Text 3')
 
-if sys.version_info < (3,):
-    from package_control.bootstrap import bootstrap_dependency, mark_bootstrapped
-    from package_control.package_manager import PackageManager
-    from package_control import loader, text, sys_path
-else:
-    from .package_control.bootstrap import bootstrap_dependency, mark_bootstrapped
-    from .package_control.package_manager import PackageManager
-    from .package_control import loader, text, sys_path
+from .package_control import loader
+from .package_control import text
+from .package_control.bootstrap import bootstrap_dependency
+from .package_control.bootstrap import mark_bootstrapped
+from .package_control.package_manager import PackageManager
 
 
 def plugin_loaded():
     manager = PackageManager()
     settings = manager.settings.copy()
-
     threading.Thread(target=_background_bootstrap, args=(settings,)).start()
 
 
@@ -45,93 +31,63 @@ def _background_bootstrap(settings):
     """
 
     base_loader_code = """
-        import sys
         import os
+        import sys
         from os.path import dirname
-
 
         # This file adds the package_control subdirectory of Package Control
         # to first in the sys.path so that all other packages may rely on
         # PC for utility functions, such as event helpers, adding things to
         # sys.path, downloading files from the internet, etc
 
-
-        if sys.version_info >= (3,):
-            def decode(path):
-                return path
-
-            def encode(path):
-                return path
-
-            loader_dir = dirname(__file__)
-
-        else:
-            def decode(path):
-                if not isinstance(path, unicode):
-                    path = path.decode(sys.getfilesystemencoding())
-                return path
-
-            def encode(path):
-                if isinstance(path, unicode):
-                    path = path.encode(sys.getfilesystemencoding())
-                return path
-
-            loader_dir = decode(os.getcwd())
-
+        loader_dir = dirname(__file__)
 
         st_dir = dirname(dirname(loader_dir))
 
         found = False
-        if sys.version_info >= (3,):
-            installed_packages_dir = os.path.join(st_dir, u'Installed Packages')
-            pc_package_path = os.path.join(installed_packages_dir, u'Package Control.sublime-package')
-            if os.path.exists(encode(pc_package_path)):
-                found = True
+        installed_packages_dir = os.path.join(st_dir, 'Installed Packages')
+        pc_package_path = os.path.join(installed_packages_dir, 'Package Control.sublime-package')
+        if os.path.exists(pc_package_path):
+            found = True
 
         if not found:
-            packages_dir = os.path.join(st_dir, u'Packages')
-            pc_package_path = os.path.join(packages_dir, u'Package Control')
-            if os.path.exists(encode(pc_package_path)):
+            packages_dir = os.path.join(st_dir, 'Packages')
+            pc_package_path = os.path.join(packages_dir, 'Package Control')
+            if os.path.exists(pc_package_path):
                 found = True
 
         # Handle the development environment
-        if not found and sys.version_info >= (3,):
+        if not found:
             import Default.sort
             if os.path.basename(Default.sort.__file__) == 'sort.py':
                 packages_dir = dirname(dirname(Default.sort.__file__))
-                pc_package_path = os.path.join(packages_dir, u'Package Control')
-                if os.path.exists(encode(pc_package_path)):
+                pc_package_path = os.path.join(packages_dir, 'Package Control')
+                if os.path.exists(pc_package_path):
                     found = True
 
         if found:
-            if os.name == 'nt':
-                from ctypes import windll, create_unicode_buffer
-                buf = create_unicode_buffer(512)
-                if windll.kernel32.GetShortPathNameW(pc_package_path, buf, len(buf)):
-                    pc_package_path = buf.value
-
-            sys.path.insert(0, encode(pc_package_path))
+            sys.path.insert(0, pc_package_path)
             import package_control
             # We remove the import path right away so as not to screw up
             # Sublime Text and its import machinery
-            sys.path.remove(encode(pc_package_path))
+            sys.path.remove(pc_package_path)
 
         else:
-            print(u'Package Control: Error finding main directory from loader')
+            print('Package Control: Error finding main directory from loader')
     """
     base_loader_code = dedent(base_loader_code).lstrip()
     loader.add_or_update('00', 'package_control', base_loader_code)
 
     # SSL support fo Linux
     if sublime.platform() == 'linux' and int(sublime.version()) < 3109:
-        linux_ssl_url = u'http://packagecontrol.io/ssl/1.0.2/ssl-linux.sublime-package'
-        linux_ssl_hash = u'23f35f64458a0a14c99b1bb1bbc3cb04794c7361c4940e0a638d40f038acd377'
-        linux_ssl_priority = u'01'
+        linux_ssl_url = 'http://packagecontrol.io/ssl/1.0.2/ssl-linux.sublime-package'
+        linux_ssl_hash = '23f35f64458a0a14c99b1bb1bbc3cb04794c7361c4940e0a638d40f038acd377'
+        linux_ssl_priority = '01'
         linux_ssl_version = '1.0.2'
 
         def linux_ssl_show_restart():
             sublime.message_dialog(text.format(
-                u'''
+                '''
                 Package Control
 
                 Package Control just installed or upgraded the missing Python
@@ -154,41 +110,5 @@ def _background_bootstrap(settings):
             )
         ).start()
 
-    # SSL support for SHA-2 certificates with ST2 on Windows
-    elif sublime.platform() == 'windows' and sys.version_info < (3,):
-        win_ssl_url = u'http://packagecontrol.io/ssl/1.0.0/ssl-windows.sublime-package'
-        win_ssl_hash = u'3c28982eb400039cfffe53d38510556adead39ba7321f2d15a6770d3ebc75030'
-        win_ssl_priority = u'01'
-        win_ssl_version = u'1.0.0'
-
-        def win_ssl_show_restart():
-            sublime.message_dialog(text.format(
-                u'''
-                Package Control
-
-                Package Control just upgraded the Python _ssl module for ST2 on
-                Windows because the bundled one does not include support for
-                modern SSL certificates.
-
-                Please restart Sublime Text to complete the upgrade.
-                '''
-            ))
-
-        threading.Thread(
-            target=bootstrap_dependency,
-            args=(
-                settings,
-                win_ssl_url,
-                win_ssl_hash,
-                win_ssl_priority,
-                win_ssl_version,
-                win_ssl_show_restart,
-            )
-        ).start()
-
     else:
-        sublime.set_timeout(mark_bootstrapped, 10)
-
-# ST2 compat
-if sys.version_info < (3,):
-    plugin_loaded()
+        mark_bootstrapped()
