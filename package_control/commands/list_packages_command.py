wbond/package_control

commit d22abdbab4e7a6f071b485f5ec49319abb986c12

Use the new sublime.QuickPanelItem for builds that have it

package_control/package_installer.py
@@ -10,6 +10,9 @@ from .package_disabler import PackageDisabler
 from .versions import version_comparable
 
 
+USE_QUICK_PANEL_ITEM = int(sublime.version()) > 4080
+
+
 class PackageInstaller(PackageDisabler):
 
     """
@@ -62,7 +65,6 @@ class PackageInstaller(PackageDisabler):
         for package in sorted(iter(packages.keys()), key=lambda s: s.lower()):
             if ignore_packages and package in ignore_packages:
                 continue
-            package_entry = [package]
             info = packages[package]
             release = info['releases'][0]
 
@@ -132,8 +134,27 @@ class PackageInstaller(PackageDisabler):
             description = info.get('description')
             if not description:
                 description = 'No description provided'
-            package_entry.append(description)
-            package_entry.append(action + extra + ' ' + re.sub('^https?://', '', info['homepage']))
+                if USE_QUICK_PANEL_ITEM:
+                    description = '<em>%s</em>' % description
+
+            homepage = info['homepage']
+            homepage_display = re.sub('^https?://', '', homepage)
+
+            if USE_QUICK_PANEL_ITEM:
+                final_line = '<em>' + action + extra + '</em>'
+                if final_line and homepage_display:
+                    final_line += ' '
+                final_line += '<a href="%s">%s</a>' % (homepage, homepage_display)
+                package_entry = sublime.QuickPanelItem(package, [description, final_line])
+            else:
+                package_entry = [package]
+                package_entry.append(description)
+                final_line = action + extra
+                if final_line and homepage_display:
+                    final_line += ' '
+                final_line += homepage_display
+                package_entry.append(final_line)
+
             package_list.append(package_entry)
         return package_list
 
@@ -149,7 +170,10 @@ class PackageInstaller(PackageDisabler):
 
         if picked == -1:
             return
-        name = self.package_list[picked][0]
+        if USE_QUICK_PANEL_ITEM:
+            name = self.package_list[picked].trigger
+        else:
+            name = self.package_list[picked][0]
 
         if name in self.disable_packages(name, 'install'):
             def on_complete():

commit 3287efd5c1a744b9b8079e5d16aaebf14a2d43a7

Fix QuickPanelItem usages
This commit fixes a regression with several commands which use quick
panel items' labels, which were broken by commit d22abdbab4e7a6f071b485f5ec49319abb986c12.

package_control/commands/list_packages_command.py
@@ -9,6 +9,8 @@ from ..show_quick_panel import show_quick_panel
 from ..package_manager import PackageManager
 from .existing_packages_command import ExistingPackagesCommand
 
+USE_QUICK_PANEL_ITEM = hasattr(sublime, 'QuickPanelItem')
+
 
 class ListPackagesCommand(sublime_plugin.WindowCommand):
 
@@ -78,7 +80,11 @@ class ListPackagesThread(threading.Thread, ExistingPackagesCommand):
 
         if picked == -1:
             return
-        package_name = self.package_list[picked][0]
+
+        if USE_QUICK_PANEL_ITEM:
+            package_name = self.package_list[picked].trigger
+        else:
+            package_name = self.package_list[picked][0]
 
         def open_dir():
             package_dir = self.manager.get_package_dir(package_name)

package_control/commands/remove_package_command.py
@@ -11,6 +11,8 @@ from ..thread_progress import ThreadProgress
 from ..package_disabler import PackageDisabler
 from ..package_manager import PackageManager
 
+USE_QUICK_PANEL_ITEM = hasattr(sublime, 'QuickPanelItem')
+
 
 class RemovePackageCommand(sublime_plugin.WindowCommand, ExistingPackagesCommand, PackageDisabler):
 
@@ -53,16 +55,20 @@ class RemovePackageCommand(sublime_plugin.WindowCommand, ExistingPackagesCommand
 
         if picked == -1:
             return
-        package = self.package_list[picked][0]
 
-        self.disable_packages(package, 'remove')
+        if USE_QUICK_PANEL_ITEM:
+            package_name = self.package_list[picked].trigger
+        else:
+            package_name = self.package_list[picked][0]
 
-        thread = RemovePackageThread(self.manager, package)
+        self.disable_packages(package_name, 'remove')
+
+        thread = RemovePackageThread(self.manager, package_name)
         thread.start()
         ThreadProgress(
             thread,
-            'Removing package %s' % package,
-            'Package %s successfully removed' % package
+            'Removing package %s' % package_name,
+            'Package %s successfully removed' % package_name
         )
 
 

package_control/commands/upgrade_all_packages_command.py
@@ -9,6 +9,8 @@ from ..thread_progress import ThreadProgress
 from ..package_installer import PackageInstaller, PackageInstallerThread
 from ..package_renamer import PackageRenamer
 
+USE_QUICK_PANEL_ITEM = hasattr(sublime, 'QuickPanelItem')
+
 
 class UpgradeAllPackagesCommand(sublime_plugin.WindowCommand):
 
@@ -50,18 +52,23 @@ class UpgradeAllPackagesThread(threading.Thread, PackageInstaller):
             time.sleep(0.7)
 
             for info in package_list:
-                if info[0] in disabled_packages:
+                if USE_QUICK_PANEL_ITEM:
+                    package_name = info.trigger
+                else:
+                    package_name = info[0]
+
+                if package_name in disabled_packages:
                     # We use a functools.partial to generate the on-complete callback in
                     # order to bind the current value of the parameters, unlike lambdas.
-                    on_complete = functools.partial(self.reenable_package, info[0])
+                    on_complete = functools.partial(self.reenable_package, package_name)
                 else:
                     on_complete = None
-                thread = PackageInstallerThread(self.manager, info[0], on_complete)
+                thread = PackageInstallerThread(self.manager, package_name, on_complete)
                 thread.start()
                 ThreadProgress(
                     thread,
-                    'Upgrading package %s' % info[0],
-                    'Package %s successfully %s' % (info[0], self.completion_type)
+                    'Upgrading package %s' % package_name,
+                    'Package %s successfully %s' % (package_name, self.completion_type)
                 )
                 thread.join()
 
@@ -69,9 +76,11 @@ class UpgradeAllPackagesThread(threading.Thread, PackageInstaller):
         # in the main thread. We then create a new background thread so that
         # the upgrade process does not block the UI.
         def disable_packages():
-            package_names = []
-            for info in package_list:
-                package_names.append(info[0])
+            if USE_QUICK_PANEL_ITEM:
+                package_names = [info.trigger for info in package_list]
+            else:
+                package_names = [info[0] for info in package_list]
+
             disabled_packages.extend(self.disable_packages(package_names, 'upgrade'))
             threading.Thread(target=do_upgrades).start()
 

package_control/commands/upgrade_package_command.py
@@ -9,6 +9,8 @@ from ..thread_progress import ThreadProgress
 from ..package_installer import PackageInstaller, PackageInstallerThread
 from ..package_renamer import PackageRenamer
 
+USE_QUICK_PANEL_ITEM = hasattr(sublime, 'QuickPanelItem')
+
 
 class UpgradePackageCommand(sublime_plugin.WindowCommand):
 
@@ -76,18 +78,22 @@ class UpgradePackageThread(threading.Thread, PackageInstaller):
 
         if picked == -1:
             return
-        name = self.package_list[picked][0]
 
-        if name in self.disable_packages(name, 'upgrade'):
+        if USE_QUICK_PANEL_ITEM:
+            package_name = self.package_list[picked].trigger
+        else:
+            package_name = self.package_list[picked][0]
+
+        if package_name in self.disable_packages(package_name, 'upgrade'):
             def on_complete():
-                self.reenable_package(name)
+                self.reenable_package(package_name)
         else:
             on_complete = None
 
-        thread = PackageInstallerThread(self.manager, name, on_complete, pause=True)
+        thread = PackageInstallerThread(self.manager, package_name, on_complete, pause=True)
         thread.start()
         ThreadProgress(
             thread,
-            'Upgrading package %s' % name,
-            'Package %s successfully %s' % (name, self.completion_type)
+            'Upgrading package %s' % package_name,
+            'Package %s successfully %s' % (package_name, self.completion_type)
         )

package_control/package_installer.py
@@ -9,8 +9,7 @@ from .package_manager import PackageManager
 from .package_disabler import PackageDisabler
 from .versions import version_comparable
 
-
-USE_QUICK_PANEL_ITEM = int(sublime.version()) > 4080
+USE_QUICK_PANEL_ITEM = hasattr(sublime, 'QuickPanelItem')
 
 
 class PackageInstaller(PackageDisabler):