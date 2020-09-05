da1d9efe1893ddfe0b1d93cb92db8358ffbc6314
DeathAxe <deathaxe82@googlemail.com>
2020-09-05 20:42:46 +0200

    Add QuickPanelItem usage to ExistingPackagesCommand
    This commit modifies the ExistingPackagesCommand to also use
    QuickPanelItems as it is not very obvious which command uses which kind
    of quick panel item, which may lead to confusion and bugs.
    
    This change fixes a regression introduced with 3287efd5c1a744b9b8079e5d16aaebf14a2d43a7,
    which caused "Package Control: Remove Package" to break, because of the
    assumption it would also use the new style of QuickPanelItems.

--- package_control/commands/existing_packages_command.py
+++ package_control/commands/existing_packages_command.py
@@ -5,6 +5,8 @@ import sublime
 
 from ..package_manager import PackageManager
 
+USE_QUICK_PANEL_ITEM = hasattr(sublime, 'QuickPanelItem')
+
 
 class ExistingPackagesCommand():
 
@@ -38,14 +40,12 @@ class ExistingPackagesCommand():
 
         package_list = []
         for package in sorted(packages, key=lambda s: s.lower()):
-            package_entry = [package]
             metadata = self.manager.get_metadata(package)
             package_dir = os.path.join(sublime.packages_path(), package)
 
             description = metadata.get('description')
             if not description:
                 description = 'No description provided'
-            package_entry.append(description)
 
             version = metadata.get('version')
             if not version and os.path.exists(os.path.join(package_dir, '.git')):
@@ -55,13 +55,21 @@ class ExistingPackagesCommand():
             else:
                 installed_version = 'v' + version if version else 'unknown version'
 
-            url = metadata.get('url')
-            if url:
-                url = '; ' + re.sub('^https?://', '', url)
-            else:
-                url = ''
+            url = metadata.get('url', '')
+            url_display = re.sub('^https?://', '', url)
 
-            package_entry.append(action + installed_version + url)
+            if USE_QUICK_PANEL_ITEM:
+                description = '<em>%s</em>' % sublime.html_format_command(description)
+                final_line = '<em>' + action + installed_version + '</em>'
+                if url_display:
+                    final_line += '; <a href="%s">%s</a>' % (url, url_display)
+                package_entry = sublime.QuickPanelItem(package, [description, final_line])
+            else:
+                final_line = action + installed_version
+                if url_display:
+                    final_line += '; ' + url_display
+                package_entry = [package, description, final_line]
+
             package_list.append(package_entry)
 
         return package_list
