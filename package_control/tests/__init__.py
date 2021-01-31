84ae5a374295b9fa373d1e38a938b7da6d741ebf
DeathAxe <deathaxe82@googlemail.com>
2021-01-31 18:12:05 +0100

    Fix github unit tests
    This commit adds an `user_agent` field to all API requests as otherwise
    api.github.com returns 403 "forbidden".
    
    Something else recovered from `next` branch from 2017 is the `cache_length` field.

--- package_control/tests/__init__.py
+++ package_control/tests/__init__.py
@@ -5,12 +5,15 @@ import re
 
 import sublime
 
+from .. import __version__
 
 LAST_COMMIT_TIMESTAMP = '2014-11-28 20:54:15'
 LAST_COMMIT_VERSION = re.sub(r'[ :\-]', '.', LAST_COMMIT_TIMESTAMP)
 
 CLIENT_ID = ''
 CLIENT_SECRET = ''
+
+USER_AGENT = 'Package Control %s Unittests' % __version__
 
 
 class StringQueue():
