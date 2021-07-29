a87112323ca5228628eb2ce20ce48843718912fe
wbond <will@wbond.net>
2021-07-28 23:28:10 -0400

    Enable running tests via CI

--- package_control/tests/_config.py
+++ package_control/tests/_config.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 from .. import __version__
@@ -6,8 +7,8 @@ from .. import __version__
 LAST_COMMIT_TIMESTAMP = '2014-11-28 20:54:15'
 LAST_COMMIT_VERSION = re.sub(r'[ :\-]', '.', LAST_COMMIT_TIMESTAMP)
 
-CLIENT_ID = ''
-CLIENT_SECRET = ''
+CLIENT_ID = os.environ.get('GH_CLIENT_ID', '')
+CLIENT_SECRET = os.environ.get('GH_CLIENT_SECRET', '')
 
 USER_AGENT = 'Package Control %s Unittests' % __version__
 
