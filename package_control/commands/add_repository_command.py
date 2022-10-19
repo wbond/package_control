ed1d431a25ad351d9d0a551870fc79fdc6b751f0
DeathAxe <deathaxe82@googlemail.com>
2022-10-19 19:07:01 +0200

    Support unattended add/remove repository
    This command extends `add_repository` and `remove_repository` command
    by:
    
    1. `url` parameter to directly add/remove channels by name from scripts
    2. `unattended` flag to suppress any error dialogs
    
    It ensures to:
    
    1. remove settings object and repositories list from memory once
       command is completed by no longer defining them as class member.
    2. re-read settings once input_panel/quick_panel are committed in case
       settings have been changed by a background process while waiting for
       user input.

--- package_control/commands/add_repository_command.py
+++ package_control/commands/add_repository_command.py
@@ -3,6 +3,7 @@ import re
 import sublime
 import sublime_plugin
 
+from ..console_write import console_write
 from ..settings import pc_settings_filename
 from ..show_error import show_error
 
@@ -11,48 +12,51 @@ class AddRepositoryCommand(sublime_plugin.ApplicationCommand):
 
     """
     A command to add a new repository to the user's Package Control settings
+
+    Example:
+
+    ```py
+    sublime.run_command(
+        "add_repository",
+        {
+            "url": "https://my-server.com/repository.json",
+            "unattended": False  # don't suppress error dialogs
+        }
+    )
+    ```
     """
 
-    def run(self):
+    def run(self, url=None, unattended=False):
+        if isinstance(url, str):
+            url = url.strip()
+
+            if re.match(r'https?://', url, re.I) is None:
+                output_fn = console_write if unattended else show_error
+                output_fn(
+                    '''
+                    Unable to add the repository "%s" since it does not appear to
+                    be served via HTTP (http:// or https://).
+                    ''',
+                    url
+                )
+                return
+
+            settings = sublime.load_settings(pc_settings_filename())
+            repositories = settings.get('repositories')
+            if not repositories:
+                repositories = []
+            elif url in repositories:
+                return
+            repositories.append(url)
+            settings.set('repositories', repositories)
+            sublime.save_settings(pc_settings_filename())
+            sublime.status_message('Repository %s successfully added' % url)
+            return
+
         sublime.active_window().show_input_panel(
             'GitHub, GitLab or BitBucket Web URL, or Custom JSON Repository URL',
             '',
-            self.on_done,
-            self.on_change,
-            self.on_cancel
+            self.run,
+            None,
+            None
         )
-
-    def on_done(self, input_text):
-        """
-        Input panel handler - adds the provided URL as a repository
-
-        :param input_text:
-            A string of the URL to the new repository
-        """
-
-        input_text = input_text.strip()
-
-        if re.match(r'https?://', input_text, re.I) is None:
-            show_error(
-                '''
-                Unable to add the repository "%s" since it does not appear to
-                be served via HTTP (http:// or https://).
-                ''',
-                input_text
-            )
-            return
-
-        settings = sublime.load_settings(pc_settings_filename())
-        repositories = settings.get('repositories', [])
-        if not repositories:
-            repositories = []
-        repositories.append(input_text)
-        settings.set('repositories', repositories)
-        sublime.save_settings(pc_settings_filename())
-        sublime.status_message('Repository %s successfully added' % input_text)
-
-    def on_change(self, input_text):
-        pass
-
-    def on_cancel(self):
-        pass
--- package_control/commands/remove_repository_command.py
+++ package_control/commands/remove_repository_command.py
@@ -9,40 +9,47 @@ class RemoveRepositoryCommand(sublime_plugin.ApplicationCommand):
 
     """
     A command to remove a repository from the user's Package Control settings
+
+    Example:
+
+    ```py
+    sublime.run_command(
+        "remove_repository",
+        {
+            "url": "https://my-server.com/repository.json",
+            "unattended": False  # don't suppress error dialogs
+        }
+    )
+    ```
     """
 
-    def run(self):
-        self.settings = sublime.load_settings(pc_settings_filename())
-        self.repositories = self.settings.get('repositories')
-        if not self.repositories:
-            show_message('There are no repositories to remove')
+    def run(self, url=None, unattended=False):
+        settings = sublime.load_settings(pc_settings_filename())
+        repositories = settings.get('repositories')
+        if not repositories:
+            if not url or not unattended:
+                show_message('There are no repositories to remove')
             return
+
+        if url:
+            try:
+                repositories.remove(url)
+            except (ValueError):
+                pass
+            else:
+                settings.set('repositories', repositories)
+                sublime.save_settings(pc_settings_filename())
+                sublime.status_message('Repository %s successfully removed' % url)
+            return
+
+        def on_done(index):
+            if index == -1:
+                return
+
+            self.run(repositories[index])
 
         sublime.active_window().show_quick_panel(
-            self.repositories,
-            self.on_done,
+            repositories,
+            on_done,
             sublime.KEEP_OPEN_ON_FOCUS_LOST
         )
-
-    def on_done(self, index):
-        """
-        Quick panel handler - removes the repository from settings
-
-        :param index:
-            The numeric index of the repository in the list of repositories
-        """
-
-        # Cancelled
-        if index == -1:
-            return
-
-        repository = self.repositories[index]
-
-        try:
-            self.repositories.remove(repository)
-            self.settings.set('repositories', self.repositories)
-            sublime.save_settings(pc_settings_filename())
-            sublime.status_message('Repository %s successfully removed' % repository)
-
-        except (ValueError):
-            pass
