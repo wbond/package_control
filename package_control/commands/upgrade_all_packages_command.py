0fa0e4ed21075f8074d6f6a0c80528eecc6de1fa
DeathAxe <deathaxe82@googlemail.com>
2022-10-23 10:40:17 +0200

    Tweak Upgrade/Overwrite All command
    This commit...
    
    1. removes none-used `window` attribute
    2. support unattended mode for scripting (no modal dialogs)
    3. skips all packages, if 'Package Control' is to be updated the same
       way as the automatic updater would do.
    4. adds a console logging
    5. reduce thread creation by following code structure of
       `upgrade_packages` command
       
       We loose some status bar messages, but they don't justify the
       insane thread creation pin-pong. May find better solutions.
    
       Creating new thread progress instances for each package install
       caused the thread progress to flicker, which is no good UX anyway.

--- package_control/commands/upgrade_all_packages_command.py
+++ package_control/commands/upgrade_all_packages_command.py
@@ -1,15 +1,7 @@
-import time
-import threading
-
-import sublime
 import sublime_plugin
 
-from ..package_installer import PackageInstaller, PackageInstallerThread
-from ..package_renamer import PackageRenamer
-from ..show_error import show_message
 from ..thread_progress import ThreadProgress
-
-USE_QUICK_PANEL_ITEM = hasattr(sublime, 'QuickPanelItem')
+from .upgrade_packages_command import UpgradePackagesThread
 
 
 class UpgradeAllPackagesCommand(sublime_plugin.ApplicationCommand):
@@ -17,65 +9,13 @@ class UpgradeAllPackagesCommand(sublime_plugin.ApplicationCommand):
     """
     A command to automatically upgrade all installed packages that are
     upgradable.
+
+    ```py
+    sublime.run_command("upgrade_all_packages", {"unattended": False})
+    ```
     """
 
-    def run(self):
-        thread = UpgradeAllPackagesThread(sublime.active_window())
+    def run(self, unattended=False):
+        thread = UpgradePackagesThread(None, unattended)
         thread.start()
-        ThreadProgress(thread, 'Loading repositories', '')
-
-
-class UpgradeAllPackagesThread(threading.Thread, PackageInstaller):
-
-    """
-    A thread to run the action of retrieving upgradable packages in.
-    """
-
-    def __init__(self, window):
-        """
-        Constructs a new instance.
-
-        :param window:
-            An instance of :class:`sublime.Window` that represents the Sublime
-            Text window to show the list of upgradable packages in.
-        """
-
-        self.window = window
-        threading.Thread.__init__(self)
-        PackageInstaller.__init__(self)
-
-    def run(self):
-        PackageRenamer().rename_packages(self.manager)
-        package_list = self.make_package_list(['install', 'reinstall', 'none'])
-        if not package_list:
-            show_message('All packages up-to-date!')
-            return
-
-        if USE_QUICK_PANEL_ITEM:
-            package_names = [info.trigger for info in package_list]
-        else:
-            package_names = [info[0] for info in package_list]
-
-        def do_upgrade():
-            disabled_packages = self.disable_packages(package_names, 'upgrade')
-
-            try:
-                # Pause so packages can be disabled
-                time.sleep(0.7)
-
-                for package_name in package_names:
-                    thread = PackageInstallerThread(self.manager, package_name, None)
-                    thread.start()
-                    ThreadProgress(
-                        thread,
-                        'Upgrading package %s' % package_name,
-                        'Package %s successfully upgraded' % package_name
-                    )
-                    thread.join()
-
-            finally:
-                time.sleep(0.7)
-                self.reenable_packages(disabled_packages, 'upgrade')
-
-        # clear "Loading repository" thread progress
-        threading.Thread(target=do_upgrade).start()
+        ThreadProgress(thread, 'Upgrading packages...', '')
