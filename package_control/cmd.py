fb18cb90b46f2c37e1b2cf42daa99ada054cd8f0
deathaxe <deathaxe82@googlemail.com>
2023-02-26 14:04:38 +0100

    Include stderr into error message lookup of cli calls

--- package_control/cmd.py
+++ package_control/cmd.py
@@ -171,7 +171,7 @@ class Cli:
                     show_error(message)
                 sublime.set_timeout(kill_proc, 60000)
 
-            output, _ = proc.communicate(input)
+            output, error = proc.communicate(input)
 
             stuck = False
 
@@ -179,7 +179,10 @@ class Cli:
             output = output.replace('\r\n', '\n').rstrip(' \n\r')
 
             if proc.returncode not in self.ok_returncodes:
-                if not ignore_errors or re.search(ignore_errors, output) is None:
+                if error:
+                    error = error.decode(encoding)
+                    error = error.replace('\r\n', '\n').rstrip(' \n\r')
+                if not ignore_errors or re.search(ignore_errors, error or output) is None:
                     message = text.format(
                         '''
                         Error executing: %s
@@ -188,8 +191,8 @@ class Cli:
 
                         %s
                         ''',
-                        (create_cmd(args), orig_cwd, output)
-                    )
+                        (create_cmd(args), orig_cwd, error or output)
+                    ).rstrip()
                     if is_vcs:
                         message += text.format(
                             '''
