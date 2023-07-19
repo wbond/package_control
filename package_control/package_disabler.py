wbond/package_control

commit 8e1132126b22d56df969809c23a813b4f4936e5f

Publish events module
Fixes #1642

The following import statement may fail:

```py
from package_control import events
```

Reasons:

1. invoked by python 3.8 plugins which load earlier than Package Control.
2. invoked by python 3.3 plugins, while Package Control is loaded in python 3.8
   environment.

In both cases python's import machinary is not able to resolve module source
path and thus fails importing.

This commit therefore modifies injector code to manually resolve and load
`events` module and adds it to `sys.modules` and `package_control`.

Note: 

Relative imports such as `from . import events` no longer work, within
Package Control package. Instead `from package_control import events` must be
used.

related with: https://github.com/timbrel/GitSavvy/issues/1576

package_control/package_disabler.py
@@ -5,7 +5,10 @@ from threading import RLock
 
 import sublime
 
-from . import events
+# Relative import may not work here due to hard loading events
+# into global package_control (see bootstrap.py)!
+from package_control import events
+
 from .console_write import console_write
 from .package_io import package_file_exists, read_package_file
 from .settings import (

package_control/bootstrap.py
@@ -133,6 +133,7 @@ def _install_injectors():
 
     injector_code = R"""
         import os
+        import sys
         import zipfile
 
         import sublime_plugin
@@ -161,10 +162,18 @@ def _install_injectors():
             except (OSError, KeyError):
                 pass
 
+            # may be required before Package Control has been loaded
+            events = sys.modules.get('package_control.events')
+            if events is None:
+                events = __loader__.load_module("Package Control.package_control.events")
+                events.__name__ = 'package_control.events'
+                events.__package__ = 'package_control'
+                sys.modules['package_control.events'] = events
+
         elif os.path.exists(__pkg_path):
             from importlib.machinery import SourceFileLoader
 
-            __file_path = os.path.join(__pkg_path,  '__init__.py')
+            __file_path = os.path.join(__pkg_path, '__init__.py')
 
             __loader__ = SourceFileLoader('package_control', __file_path)
 
@@ -173,6 +182,14 @@ def _install_injectors():
                     __code = compile(__f.read(), '__init__.py', 'exec')
             except (OSError):
                 pass
+
+            # may be required before Package Control has been loaded
+            events = sys.modules.get('package_control.events')
+            if events is None:
+                events = SourceFileLoader('events', os.path.join(__pkg_path, 'events.py')).load_module()
+                events.__name__ = 'package_control.events'
+                events.__package__ = 'package_control'
+                sys.modules['package_control.events'] = events
 
             del globals()['SourceFileLoader']
 
@@ -191,6 +208,7 @@ def _install_injectors():
         del globals()['__data_path']
         del globals()['sublime_plugin']
         del globals()['zipfile']
+        del globals()['sys']
         del globals()['os']
 
         __data = {}

commit 85c65903406b4704fac3604c5012fdb867b9dda6

Fix import error if bootstrapping is not yet completed
Fixes #1644

If package_control injector code has not yet been written (first start with
PC4.0 installed), an import error was thown due to missing module.

Regressed by 8e1132126b22d56df969809c23a813b4f4936e5f

This commit therefore falls back to relative import, in case bootstrapping is
not yet completed.

package_control/package_disabler.py
@@ -5,9 +5,13 @@ from threading import RLock
 
 import sublime
 
-# Relative import may not work here due to hard loading events
-# into global package_control (see bootstrap.py)!
-from package_control import events
+try:
+    # Relative import does not work here due to hard loading events
+    # into global package_control (see bootstrap.py)!
+    from package_control import events
+except ImportError:
+    # use relative import, if bootstrapping has not yet been completed
+    from . import events
 
 from .console_write import console_write
 from .package_io import package_file_exists, read_package_file