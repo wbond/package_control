f9b16dd8b674dce041cbadf4bea02861da9899a5
deathaxe <deathaxe82@googlemail.com>
2023-08-20 12:02:10 +0200

    Exclude unittests from production builds

--- package_control/commands/package_control_tests_command.py
+++ package_control/commands/package_control_tests_command.py
@@ -2,16 +2,21 @@ import sublime
 import sublime_plugin
 
 from ..settings import pc_settings_filename
-from ..tests import TestRunner, TEST_CLASSES
 
+try:
+    from ..tests import TestRunner, TEST_CLASSES
+except ImportError:
+    class PackageControlTestsCommand:
+        pass
 
-class PackageControlTestsCommand(sublime_plugin.ApplicationCommand):
-    """
-    A command to run the tests for Package Control
-    """
+else:
+    class PackageControlTestsCommand(sublime_plugin.ApplicationCommand):
+        """
+        A command to run the tests for Package Control
+        """
 
-    def run(self):
-        TestRunner(args=(sublime.active_window(), TEST_CLASSES)).start()
+        def run(self):
+            TestRunner(args=(sublime.active_window(), TEST_CLASSES)).start()
 
-    def is_visible(self):
-        return sublime.load_settings(pc_settings_filename()).get('enable_tests', False)
+        def is_visible(self):
+            return sublime.load_settings(pc_settings_filename()).get("enable_tests", False)
