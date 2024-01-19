d4b43a72471a07c03b6044918da0a32faa995afa
deathaxe <deathaxe82@googlemail.com>
2024-01-19 19:02:54 +0100

    UnitTest: simplify sublime API mocking

--- package_control/tests/__init__.py
+++ package_control/tests/__init__.py
@@ -1,19 +1,10 @@
-from sys import modules
-
-if "sublime" not in modules:
-    import importlib.machinery
-    import os
-
-    PACKAGE_ROOT = os.path.dirname(__file__)
-
-    # Mock the sublime module for CLI usage
-    sublime = importlib.machinery.SourceFileLoader(
-        "sublime",
-        os.path.join(PACKAGE_ROOT, "mock_sublime.py")
-    ).load_module()
-
-    # Mock the sublime_plugin module for CLI usage
-    sublime_plugin = importlib.machinery.SourceFileLoader(
-        "sublime_plugin",
-        os.path.join(PACKAGE_ROOT, "mock_sublime_plugin.py")
-    ).load_module()
+# flake8: noqa: F401
+try:
+    import sublime
+except ImportError:
+    # Mock the sublime API modules for CLI usage
+    from sys import modules
+    from . import mock_sublime
+    from . import mock_sublime_plugin
+    modules["sublime"] = mock_sublime
+    modules["sublime_plugin"] = mock_sublime_plugin
