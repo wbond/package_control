wbond/package_control

commit 598661854daca79773b4683388cfc03456e32c64

Fix PEP491 compliant library name matching
Fixes #1667

1. matches libraries using lower-case PEP491 escaped names
2. fixes Library.__lt__() not using `rhs` to compare against.

package_control/package_manager.py
@@ -239,13 +239,7 @@ class PackageManager:
         if not names:
             return set()
 
-        builtin = library.builtin_libraries(python_version)
-
-        return set(
-            library.Library(library.translate_name(name), python_version)
-            for name in names
-            if name not in builtin
-        )
+        return set(library.names_to_libraries(names, python_version))
 
     def get_python_version(self, package_name):
         """
@@ -536,9 +530,9 @@ class PackageManager:
                                 # in scheme 4.0.0. Do it here to apply only on client side.
                                 name = info['name'] = library.translate_name(name)
 
-                                info['releases'] = self.select_releases(name, info['releases'])
                                 if info['releases']:
-                                    filtered_libraries[name] = info
+                                    dist_name = library.escape_name(name).lower()
+                                    filtered_libraries[dist_name] = info
                                 else:
                                     unavailable_libraries.append(name)
 
@@ -672,7 +666,8 @@ class PackageManager:
 
                 info['releases'] = self.select_releases(name, info['releases'])
                 if info['releases']:
-                    repository_libraries[name] = info
+                    dist_name = library.escape_name(name).lower()
+                    repository_libraries[dist_name] = info
                 else:
                     unavailable_libraries.append(name)
 
@@ -1164,7 +1159,7 @@ class PackageManager:
         release = None
         available_version = None
 
-        available_library = self.list_available_libraries().get(lib.name)
+        available_library = self.list_available_libraries().get(lib.dist_name)
         if available_library:
             for available_release in available_library['releases']:
                 if lib.python_version in available_release['python_versions']:
@@ -1244,7 +1239,7 @@ class PackageManager:
             # search '<name>-<version>.dist-info/RECORD' directory in archive
             # be permissive with version part as it may have a different format as `available_version`
             new_did_name = None
-            pattern = re.compile(r'({0.name}-\S+\.dist-info)/RECORD'.format(lib))
+            pattern = re.compile(r'({0.dist_name}-\S+\.dist-info)/RECORD'.format(lib), re.IGNORECASE)
             for i in library_zip.infolist():
                 match = pattern.match(i.filename)
                 if match:
@@ -1577,13 +1572,8 @@ class PackageManager:
                 library_names = self.select_libraries(lib_info)
 
             if library_names:
-                builtin = library.builtin_libraries(python_version)
                 self.install_libraries(
-                    (
-                        library.Library(library.translate_name(name), python_version)
-                        for name in library_names
-                        if name not in builtin
-                    ),
+                    library.names_to_libraries(library_names, python_version),
                     fail_early=False
                 )
 

package_control/distinfo.py
@@ -1,28 +1,11 @@
 import base64
 import hashlib
 import os
-import re
 import sys
 
 from . import __version__ as pc_version
 from . import pep440
 from . import sys_path
-
-PEP491_NAME_PATTERN = re.compile(r"[^\w\d.]+", re.UNICODE)
-"""PEP491 package name escape pattern."""
-
-
-def escape_name(library_name):
-    """
-    Escape library name according to PEP491
-
-    :param library_name:
-        library name
-
-    :returns:
-        PEP491 escaped distribution name
-    """
-    return PEP491_NAME_PATTERN.sub("_", library_name)
 
 
 def _trim_segments(rel_path, segments):
@@ -148,17 +131,6 @@ class DistInfoDir:
         Return the absolute path of the distinfo directory on filesystem.
         """
         return os.path.join(self.install_root, self.dir_name)
-
-    @property
-    def library_name(self):
-        """
-        Return the library name.
-
-        :returns:
-            Unescaped library name.
-        """
-        meta = self.read_metadata()
-        return meta["name"]
 
     def exists(self):
         """

package_control/library.py
@@ -1,4 +1,5 @@
 import os
+import re
 import shutil
 
 import sublime
@@ -7,7 +8,7 @@ from . import sys_path
 from . import distinfo
 from .clear_directory import delete_directory
 
-BUILTIN_38_LIBRARIES = {
+BUILTIN_LIBRARIES = {
     "3.3": {},
     "3.8": {"enum", "pathlib", "typing"}
 }
@@ -34,35 +35,61 @@ which still request old dependencies are pointed to the new ones,
 which should reduce friction when moving on to python 3.8 onwards.
 """
 
+PEP491_NAME_PATTERN = re.compile(r"[^\w.]+", re.UNICODE)
+"""PEP491 package name escape pattern."""
 
-def builtin_libraries(python_version):
+
+def escape_name(name):
     """
-    Determine built-in library names, which were 3rd-party libraries before
+    Escape library name according to PEP491
 
-    :param python_version:
-        The python version to return built-in libraries for
+    :param name:
+        library name
 
     :returns:
-        A set of library names.
+        PEP491 escaped distribution name
     """
-    return BUILTIN_38_LIBRARIES.get(python_version, set())
+    return PEP491_NAME_PATTERN.sub("_", name)
 
 
-def translate_name(dependency_name):
+def translate_name(name):
     """
     Translate old dependency name to real pypi library name
 
-    :param dependency_name:
+    :param name:
         Legacy Sublime Text dependency name
 
     :returns:
         PyPI complient library name.
     """
-    return DEPENDENCY_NAME_MAP.get(dependency_name, dependency_name)
+    return DEPENDENCY_NAME_MAP.get(name, name)
+
+
+def names_to_libraries(names, python_version):
+    """
+    Convert a set of dependency names into libraries.
+
+    :param names:
+        The iteratable of possibly legacy dependency or library names.
+
+    :param python_version:
+        The python version to built the set for.
+
+    :returns:
+        A generator object of ``Library`` objects.
+    """
+    builtins = BUILTIN_LIBRARIES.get(python_version, set())
+
+    for name in names:
+        name = translate_name(name)
+        if name not in builtins:
+            yield Library(name, python_version)
+
+    return None
 
 
 class Library:
-    __slots__ = ['name', 'python_version']
+    __slots__ = ['name', 'dist_name', 'python_version']
 
     def __init__(self, name, python_version):
         if not isinstance(name, str):
@@ -78,13 +105,14 @@ class Library:
             )
 
         self.name = name
+        self.dist_name = escape_name(self.name).lower()
         self.python_version = python_version
 
     def __repr__(self):
-        return "Library(%r, %r)" % (self.name, self.python_version)
+        return "{}({!r}, {!r})".format(self.__class__.__name__, self.name, self.python_version)
 
-    def _to_tuple(self, lower=False):
-        return (self.name.lower() if lower else self.name, self.python_version)
+    def __str__(self):
+        return self.name
 
     def __hash__(self):
         return hash(self._to_tuple())
@@ -95,24 +123,24 @@ class Library:
     def __ne__(self, rhs):
         return self._to_tuple() != rhs._to_tuple()
 
-    def __lt__(self, rhs):
-        """
-        Default sorting is case insensitive
-        """
+    def __gt__(self, rhs):
+        return self._to_tuple() > rhs._to_tuple()
 
-        self_lt = self._to_tuple(lower=True)
-        rhs_lt = self._to_tuple(lower=True)
-        if self_lt == rhs_lt:
-            return self._to_tuple() < rhs._to_tuple()
-        return self_lt < rhs_lt
+    def __lt__(self, rhs):
+        return self._to_tuple() < rhs._to_tuple()
+
+    def _to_tuple(self):
+        return (self.dist_name, self.python_version)
 
 
 class InstalledLibrary(Library):
     __slots__ = ['dist_info']
 
-    def __init__(self, dist_info_dir, python_version):
-        self.dist_info = distinfo.DistInfoDir(sys_path.lib_paths()[python_version], dist_info_dir)
-        super().__init__(self.dist_info.library_name, python_version)
+    def __init__(self, install_root, dist_info_dir, python_version):
+        self.dist_info = distinfo.DistInfoDir(install_root, dist_info_dir)
+        self.name = self.dist_info.read_metadata()["name"]
+        self.dist_name = dist_info_dir[:dist_info_dir.find("-")].lower()
+        self.python_version = python_version
 
 
 def list_all():
@@ -131,7 +159,7 @@ def list_all():
             record_path = os.path.join(install_root, fname, 'RECORD')
             if not os.path.isfile(record_path):
                 continue
-            out.add(InstalledLibrary(fname, python_version))
+            out.add(InstalledLibrary(install_root, fname, python_version))
 
     return out
 
@@ -147,7 +175,7 @@ def list_unmanaged():
     out = set()
     for python_version, install_root in sys_path.lib_paths().items():
         for fname in os.listdir(install_root):
-            if not fname.endswith(".dist-info"):
+            if not fname.lower().endswith(".dist-info"):
                 continue
             installer_path = os.path.join(install_root, fname, 'INSTALLER')
             if not os.path.isfile(installer_path):
@@ -158,7 +186,7 @@ def list_unmanaged():
                 if f.read().strip().startswith('Package Control'):
                     continue
 
-            out.add(InstalledLibrary(fname, python_version))
+            out.add(InstalledLibrary(install_root, fname, python_version))
 
     return out
 
@@ -176,11 +204,12 @@ def find_installed(lib):
     returns:
         An InstalledLibrary() object
     """
+    pattern = re.compile(r"{0.dist_name}-\S+\.dist-info".format(lib), re.IGNORECASE)
+    install_root = sys_path.lib_paths()[lib.python_version]
+    for fname in os.listdir(install_root):
+        if pattern.match(fname):
+            return InstalledLibrary(install_root, fname, lib.python_version)
 
-    dist_pattern = distinfo.escape_name(lib.name) + "-"
-    for fname in os.listdir(sys_path.lib_paths()[lib.python_version]):
-        if fname.startswith(dist_pattern) and fname.endswith(".dist-info"):
-            return InstalledLibrary(fname, lib.python_version)
     return None
 
 
@@ -259,7 +288,7 @@ def convert_dependency(dependency_path, python_version, name, version, descripti
     if not src_dir:
         raise ValueError('Unrecognized or incompatible source archive layout')
 
-    did_name = '%s-%s.dist-info' % (distinfo.escape_name(name), version)
+    did_name = '%s-%s.dist-info' % (escape_name(name), version)
     did = distinfo.DistInfoDir(src_dir, did_name)
     did.ensure_exists()
     did.write_metadata(name, version, description, url)

package_control/tests/test_library.py
@@ -1,0 +1,115 @@
+import unittest
+
+from .. import library
+from ._data_decorator import data_decorator, data
+
+
+@data_decorator
+class LibraryTests(unittest.TestCase):
+    @data(
+        (
+            ("testlib", "testlib"),
+            ("test-lib", "test_lib"),
+            ("Test-liB", "Test_liB"),
+            ("test_lib", "test_lib"),
+            ("test.lib", "test.lib"),
+            ("test.0815", "test.0815"),
+            ("test!0815", "test_0815"),
+            ("test?0815", "test_0815"),
+            ("test=0815", "test_0815"),
+        )
+    )
+    def escape_name(self, name, result):
+        self.assertEqual(result, library.escape_name(name))
+
+    @data(
+        (
+            # legacy dependencies are translated
+            ("bs4", "beautifulsoup4"),
+            ("dateutil", "python-dateutil"),
+            ("python-jinja2", "Jinja2"),
+            ("python-markdown", "Markdown"),
+            ("python-pywin32", "pywin32"),
+            ("python-six", "six"),
+            ("python-toml", "toml"),
+            ("pyyaml", "PyYAML"),
+            ("ruamel-yaml", "ruamel.yaml"),
+            ("serial", "pyserial"),
+            # normal python packages keep unchanged
+            ("Markdown", "Markdown"),
+            ("markdown", "markdown"),
+            ("python-docx", "python-docx"),
+            ("ruamel.yaml", "ruamel.yaml"),
+            # check no escaping being performed
+            ("testlib", "testlib"),
+            ("test-lib", "test-lib"),
+            ("Test-liB", "Test-liB"),
+            ("test_lib", "test_lib"),
+            ("test.lib", "test.lib"),
+            ("test.0815", "test.0815"),
+            ("test!0815", "test!0815"),
+            ("test?0815", "test?0815"),
+            ("test=0815", "test=0815"),
+        )
+    )
+    def translate_name(self, name, result):
+        self.assertEqual(result, library.translate_name(name))
+
+    def test_names_to_libraries(self):
+        self.assertEqual(
+            [
+                library.Library(n, "3.8")
+                for n in ("beautifulsoup4", "python-dateutil", "python-docx")
+            ],
+            sorted(
+                library.names_to_libraries(["bs4", "dateutil", "pathlib", "python-docx"], "3.8")
+            ),
+        )
+
+    @data(
+        (
+            (("test-lib", "3.3"), ("test-lib", "3.3")),
+            (("test-lib", "3.3"), ("test_lib", "3.3")),
+            (("test-lib", "3.3"), ("Test_lib", "3.3")),
+            (("test lib", "3.3"), ("test_lib", "3.3")),
+            (("Test-Lib", "3.3"), ("test_lib", "3.3")),
+            (("Test-Lib", "3.3"), ("test-lib", "3.3")),
+            (("Test!Lib", "3.3"), ("test?lib", "3.3")),
+            (("Test!Lib", "3.3"), ("test_lib", "3.3")),
+        )
+    )
+    def library_equal(self, a, b):
+        self.assertEqual(library.Library(*a), library.Library(*b))
+
+    @data(
+        (
+            (("test-lib", "3.3"), ("test-lib", "3.8")),
+            (("test-lib", "3.3"), ("test.lib", "3.3")),
+            (("test_lib", "3.3"), ("test.lib", "3.3")),
+            (("testlib", "3.3"), ("test.lib", "3.3")),
+            (("testlib", "3.3"), ("test_lib", "3.3")),
+            (("testlib", "3.3"), ("test-lib", "3.3")),
+        )
+    )
+    def library_not_equal(self, a, b):
+        self.assertNotEqual(library.Library(*a), library.Library(*b))
+
+    @data(
+        (
+            (("test-lib", "3.3"), ("test-lib", "3.8")),
+            (("test-lib-1", "3.3"), ("test-lib-2", "3.3")),
+            (("test-lib-a", "3.3"), ("test-lib-b", "3.3")),
+        )
+    )
+    def library_lesser(self, a, b):
+        self.assertLess(library.Library(*a), library.Library(*b))
+
+    @data(
+        (
+            (("test-lib", "3.8"), ("test-lib", "3.3")),
+            (("test-lib-2", "3.3"), ("test-lib-1", "3.3")),
+            (("test-lib-b", "3.3"), ("test-lib-a", "3.3")),
+        )
+    )
+    def library_greater(self, a, b):
+        self.assertGreater(library.Library(*a), library.Library(*b))

commit c9634b19cf0a632cea1a0865092e1a5a4ddc4530

Fix wrong platform-specific library being installed
This commit fixes a regression introduced by 598661854daca79773b4683388cfc03456e32c64.

package_control/package_manager.py
@@ -530,6 +530,7 @@ class PackageManager:
                                 # in scheme 4.0.0. Do it here to apply only on client side.
                                 name = info['name'] = library.translate_name(name)
 
+                                info['releases'] = self.select_releases(name, info['releases'])
                                 if info['releases']:
                                     dist_name = library.escape_name(name).lower()
                                     filtered_libraries[dist_name] = info