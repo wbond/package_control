d291cfa3963bf733c479d7d040124c26588bb798
herr kaste <herr.kaste@gmail.com>
2025-05-18 21:24:56 +0200

    Merge "channel_v4" and "channel_v3"

--- scripts/generate_channel.py
+++ scripts/generate_channel.py
@@ -1,20 +1,36 @@
 from __future__ import annotations
 
+from itertools import chain
 import json
 import os
 import urllib.request
 
 
-INPUT_FILE = "https://packagecontrol.io/channel_v3.json"
+v3_CHANNEL = "https://packagecontrol.io/channel_v3.json"
+v4_CHANNEL = "https://packagecontrol.github.io/channel/channel_v4.json"
 OUTPUT_FILE = os.path.abspath("channel.json")
 
 
 def main():
-    channel = http_get_json(INPUT_FILE)
+    v3_channel = http_get_json(v3_CHANNEL)
+    v4_channel = http_get_json(v4_CHANNEL)
+
+    dependencies = v3_channel.pop('dependencies_cache', {})
+    for library in chain(*dependencies.values()):
+        del library['load_order']
+        for release in library['releases']:
+            release['python_versions'] = ['3.3']
+
+    channel = {
+        "schema_version": "4.0.0",
+        "repositories": v4_channel["repositories"] + v3_channel["repositories"],
+        "packages_cache": v3_channel["packages_cache"] | v4_channel["packages_cache"],
+        "libraries_cache": dependencies | v4_channel["libraries_cache"],
+    }
+
     for key, value in channel.items():
         match key:
-            case "repositories": value.sort()
-            case "packages_cache" | "dependencies_cache":
+            case "packages_cache" | "libraries_cache":
                 for repo_url, packages in value.items():
                     packages.sort(key=lambda p: p["name"])
                     for p in packages:
