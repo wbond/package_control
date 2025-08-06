wbond/package_control

commit 968098bf23c1ef45d37ad7719e3bd6c9526041c3

Fix in_process_packages not being cleaned up at startup
A settings name mismatch caused pending package installs/upgrades not being
finished or aborted after ST startup due to settings name mismatch.

package_control/package_disabler.py
@@ -23,6 +23,9 @@ from .settings import (
 from .show_error import show_error
 from .sys_path import pc_cache_dir
 
+IGNORED_PACKAGES = 'ignored_packages'
+IN_PROCESS_PACKAGES = 'in_process_packages'
+
 
 class PackageDisabler:
 
@@ -169,13 +172,13 @@ class PackageDisabler:
     def ignored_packages():
         with PackageDisabler.lock:
             settings = sublime.load_settings(preferences_filename())
-            return load_list_setting(settings, 'ignored_packages')
+            return load_list_setting(settings, IN_PROCESS_PACKAGES)
 
     @staticmethod
-    def in_progress_packages():
+    def in_process_packages():
         with PackageDisabler.lock:
             settings = sublime.load_settings(pc_settings_filename())
-            return load_list_setting(settings, 'in_progress_packages')
+            return load_list_setting(settings, IN_PROCESS_PACKAGES)
 
     @staticmethod
     def get_version(package):
@@ -232,11 +235,11 @@ class PackageDisabler:
 
         with PackageDisabler.lock:
             settings = sublime.load_settings(preferences_filename())
-            ignored_at_start = load_list_setting(settings, 'ignored_packages')
+            ignored_at_start = load_list_setting(settings, IN_PROCESS_PACKAGES)
             ignored = set()
 
             pc_settings = sublime.load_settings(pc_settings_filename())
-            in_process_at_start = load_list_setting(pc_settings, 'in_process_packages')
+            in_process_at_start = load_list_setting(pc_settings, IN_PROCESS_PACKAGES)
             in_process = set()
 
             need_restore = False
@@ -287,7 +290,7 @@ class PackageDisabler:
             save_list_setting(
                 pc_settings,
                 pc_settings_filename(),
-                'in_process_packages',
+                IN_PROCESS_PACKAGES,
                 in_process,
                 in_process_at_start
             )
@@ -295,7 +298,7 @@ class PackageDisabler:
             save_list_setting(
                 settings,
                 preferences_filename(),
-                'ignored_packages',
+                IN_PROCESS_PACKAGES,
                 ignored,
                 ignored_at_start
             )
@@ -326,10 +329,10 @@ class PackageDisabler:
 
         with PackageDisabler.lock:
             settings = sublime.load_settings(preferences_filename())
-            ignored = load_list_setting(settings, 'ignored_packages')
+            ignored = load_list_setting(settings, IN_PROCESS_PACKAGES)
 
             pc_settings = sublime.load_settings(pc_settings_filename())
-            in_process = load_list_setting(pc_settings, 'in_process_packages')
+            in_process = load_list_setting(pc_settings, IN_PROCESS_PACKAGES)
 
             need_restore = False
             affected = set()
@@ -372,14 +375,14 @@ class PackageDisabler:
                 save_list_setting(
                     pc_settings,
                     pc_settings_filename(),
-                    'in_process_packages',
+                    IN_PROCESS_PACKAGES,
                     in_process - affected
                 )
 
                 save_list_setting(
                     settings,
                     preferences_filename(),
-                    'ignored_packages',
+                    IN_PROCESS_PACKAGES,
                     ignored - affected,
                     ignored
                 )

package_control/package_cleanup.py
@@ -77,7 +77,7 @@ class PackageCleanup(threading.Thread, PackageTaskRunner):
         # Make sure we didn't accidentally ignore packages because something was
         # interrupted before it completed. Keep orphaned packages disabled which
         # are deferred to next start.
-        in_process = self.in_progress_packages() - removed_packages
+        in_process = self.in_process_packages() - removed_packages
         if in_process:
             console_write(
                 'Re-enabling %d package%s after a Package Control operation was interrupted...',
@@ -127,7 +127,7 @@ class PackageCleanup(threading.Thread, PackageTaskRunner):
 
         message = ''
 
-        in_process = self.in_progress_packages()
+        in_process = self.in_process_packages()
         if in_process:
             message += 'to complete pending package operations on "%s"' \
                 % '", "'.join(sorted(in_process, key=lambda s: s.lower()))

commit 873efbd46658cb96e9f20dc196e5492c140b7a01

Fix broken enable/disable package commands
Fixes `ignored_packages` not being taken into account, correctly.

Broken by commit 968098bf23c1ef45d37ad7719e3bd6c9526041c3.

package_control/package_disabler.py
@@ -172,7 +172,7 @@ class PackageDisabler:
     def ignored_packages():
         with PackageDisabler.lock:
             settings = sublime.load_settings(preferences_filename())
-            return load_list_setting(settings, IN_PROCESS_PACKAGES)
+            return load_list_setting(settings, IGNORED_PACKAGES)
 
     @staticmethod
     def in_process_packages():
@@ -235,7 +235,7 @@ class PackageDisabler:
 
         with PackageDisabler.lock:
             settings = sublime.load_settings(preferences_filename())
-            ignored_at_start = load_list_setting(settings, IN_PROCESS_PACKAGES)
+            ignored_at_start = load_list_setting(settings, IGNORED_PACKAGES)
             ignored = set()
 
             pc_settings = sublime.load_settings(pc_settings_filename())
@@ -298,7 +298,7 @@ class PackageDisabler:
             save_list_setting(
                 settings,
                 preferences_filename(),
-                IN_PROCESS_PACKAGES,
+                IGNORED_PACKAGES,
                 ignored,
                 ignored_at_start
             )
@@ -329,7 +329,7 @@ class PackageDisabler:
 
         with PackageDisabler.lock:
             settings = sublime.load_settings(preferences_filename())
-            ignored = load_list_setting(settings, IN_PROCESS_PACKAGES)
+            ignored = load_list_setting(settings, IGNORED_PACKAGES)
 
             pc_settings = sublime.load_settings(pc_settings_filename())
             in_process = load_list_setting(pc_settings, IN_PROCESS_PACKAGES)
@@ -382,7 +382,7 @@ class PackageDisabler:
                 save_list_setting(
                     settings,
                     preferences_filename(),
-                    IN_PROCESS_PACKAGES,
+                    IGNORED_PACKAGES,
                     ignored - affected,
                     ignored
                 )